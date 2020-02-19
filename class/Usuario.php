f855a9c2725f2b6b2b7a3f43d56e0f68b38fc02b
wpj09 <wpjwpj09@gmail.com>
2020-02-19 20:39:53 -0300

    metodos de lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -60,6 +60,50 @@ class Usuario{
 
 	}
 
+	public static function getList(){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_ususarios ORDER BY deslogin");
+
+	}
+
+	public static function search($login){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_ususarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+			':SEARCH'=>"%".$login."%"
+		));
+
+	}
+
+	public function Login($login, $password){
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_ususarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+			":LOGIN"=>$login,
+			":PASSWORD"=>$password
+		));
+
+		if(count($results) > 0){
+
+			$row = $results[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+		} else{
+
+			throw new Exception("Login e/ou senha invalidos");
+			
+		}
+
+	}
+
 	public function __toString(){
 
 		return json_encode(array(
--- index.php
+++ index.php
@@ -2,10 +2,24 @@
 
 require_once("config.php");
 
-$root = new Usuario();
-
+//carrega um usuário
+/*$root = new Usuario();
 $root->loadById(5);
 
-echo $root;
+echo $root;*/
+
+//carrega uma lista de usuarios
+/*$lista = Usuario::getList();
+echo json_encode($lista);*/
+
+//carrega uma lista de usuarios buscando pelo login
+/*$search = Usuario::search("jo");
+echo json_encode($search);*/
+
+//carrefa um ususario usando um login e a senha
+$usuario = new Usuario();
+$usuario->Login("jose", "123");
+
+echo $usuario;
 
  ?>
